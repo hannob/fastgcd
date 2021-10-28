9732099b0df177a6fe3923fc7790163294678ebb
Hanno Böck <hanno@gentoo.org>
2021-10-28 21:12:59 +0200

    Update to gmp 6.2.1

--- install.sh
+++ install.sh
@@ -7,31 +7,31 @@ __exists() {
 get="fetch";
 ! __exists fetch && get="curl -OL";
 
-if [ ! -d gmp-6.1.2 ];
+GMP="gmp-6.2.1"
+if [ ! -d $GMP ];
 then
 
-    if [ ! -f gmp-6.1.2.tar.bz2 ];
+    if [ ! -f $GMP.tar.xz ];
     then
-        $get https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2
+        $get https://gmplib.org/download/gmp/$GMP.tar.xz
     fi
 
-    sum=`openssl sha256 gmp-6.1.2.tar.bz2 | awk -F' ' '{print $2}'`
+    sum=`openssl sha256 $GMP.tar.xz | awk -F' ' '{print $2}'`
 
-    if [[ $sum != "5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2" ]];
+    if [[ $sum != "fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2" ]];
     then
         echo ''
         echo '=========================================='
-        echo 'ERROR: could not verify gmp-6.1.2.tar.bz2;'
-        echo 'Downloaded over untrusted channel (non-https)'
+        echo 'ERROR: could not verify gmp-6.2.1.tar.xz;'
         echo '=========================================='
         exit;
     fi
 
-    tar xf gmp-6.1.2.tar.bz2
+    tar xf $GMP.tar.xz
 fi
 
-cd gmp-6.1.2
-patch -p 1 < ../gmp-6.1.2.patch
+cd $GMP
+patch -p 1 < ../$GMP.patch
 mkdir ../gmp-patched
 ./configure --prefix=$PWD/../gmp-patched/
 make -j4
